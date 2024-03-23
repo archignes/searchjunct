724019598f1c04b816d9b796ce16de97bab2d752
danielsgriffin <danielsgriffin@gmail.com>
2024-03-23 00:10:24 -0700

    fixed logo image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { GitHubLogoIcon, TwitterLogoIcon, LinkedInLogoIcon } from '@radix-ui/rea
 import { Button } from "./shadcn-ui/button";
 import { Input } from ".//shadcn-ui//input";
 import Image from 'next/image';
-const lastUpdated = "2024-03-23T07:04:48.071Z";
+const lastUpdated = "2024-03-23T07:10:24.431Z";
 
 const Footer: React.FC = () => {
   return (
